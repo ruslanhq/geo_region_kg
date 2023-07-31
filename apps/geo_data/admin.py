c3c81f0b5d7f9ac0ae32e8018da11106ad43a5df
ruslanhq <h.ruslan97@gmail.com>
2023-07-31 17:12:28 +0600

    register models in admin.py

--- apps/geo_data/admin.py
+++ apps/geo_data/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from apps.geo_data.models import Region, District, Canton, Contour
 
-# Register your models here.
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
+
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('title', 'region')
+    list_filter = ('region',)
+    search_fields = ('title',)
+
+
+@admin.register(Canton)
+class CantonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'district')
+    list_filter = ('district__region', 'district')
+    search_fields = ('title',)
+
+
+@admin.register(Contour)
+class ContourAdmin(admin.ModelAdmin):
+    list_display = ('id', 'canton')
+    list_filter = ('canton__district__region', 'canton__district', 'canton')
